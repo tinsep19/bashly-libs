f95003dbccf1fc73ffd7adbe9ce08fd19e9f001a
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2024-03-08 19:26:06 +0900

    add marker @bashly-upgrade

--- lib/base64safe.sh
+++ lib/base64safe.sh
@@ -1,3 +1,4 @@
+## [@bashly-upgrade]
 # echo "some-text" | base64safe
 function base64safe() {
     cat - | base64 -w 0 | tr '+/' '-_' | tr -d "="
--- lib/password.sh
+++ lib/password.sh
@@ -1,4 +1,4 @@
-
+## [@bashly-upgrade]
 # make_password SIZE
 function make_password() { 
   echo $(head /dev/urandom | tr -dc 'A-Za-z0-9!#$%&()\*\+\-./:;<=>?@[\]^_`{|}~' | head -c "${1:-16}") 
